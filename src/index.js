69ced2dad9e3cdf113a05d82cc3e6f1f308eb137
timebuffer <dogankaraarslan@icloud.com>
2024-09-23 01:29:27 +0200

    Fix case sensitivity for App.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; // If you have a CSS file, keep this. If not, you can remove it.
-import App from './App';  // Ensure this matches the exact name of the App component file
+import App from './app';  // Ensure this matches the exact name of the 
+
 
 ReactDOM.render(
   <React.StrictMode>
